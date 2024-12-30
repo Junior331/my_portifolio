43e6817538de58c82f5a3e7550fab87f0c233cb9
Junior331 <jarilson.rk@gmail.com>
2024-12-30 12:50:10 -0300

    Fix

--- src/app/services/mocks/index.ts
+++ src/app/services/mocks/index.ts
@@ -1,9 +1,5 @@
-import { slugs } from "./slugs";
 import { projects } from "./projects";
-import { timelineList } from "./timelineList";
 
 export const mocks = {
-  slugs,
   projects,
-  timelineList,
 };
